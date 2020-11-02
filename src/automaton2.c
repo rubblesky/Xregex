4be82b7d06ed96ce56e6be1192d36b5b4f136079
rubblesky <2018211896@mail.hfut.edu.cn>
2020-11-02 09:46:11 +0800

    debug

--- src/automaton2.c
+++ src/automaton2.c
@@ -6,6 +6,8 @@
 #define DEFAULE_MATCH_CONTENT_SIZE 10
 typedef int LongChar;
 enum MatchMode {
+    /*这里要改 不允许出现sequence 一律拆分成多条边
+    range最大值和最小值不能相同*/
     SEQUENCE,
     RANGE,
     REGEX,
@@ -27,16 +29,18 @@ struct Edge {
 
 typedef struct Status {
     Edge *outEdge;
-
+    int isEnd;
 } Status;
 
 typedef struct AutoMaton {
     Vector *status;
+    Status *start;
 
 } AutoMaton;
 
 void StatusAssign(Status *status, Status *fromStatus) {
     status->outEdge = fromStatus->outEdge;
+    status->isEnd = fromStatus->isEnd;
 }
 void freeStatusAssign(Status *s) {
     return;
@@ -48,6 +52,7 @@ AutoMaton *initAutoMaton(int statusNum) {
         return NULL;
     } else {
         am->status = INIT_VECTOR(statusNum, Status, StatusAssign, freeStatusAssign);
+        am->start = NULL;
         if (am->status == NULL) {
             free(am);
             return NULL;
@@ -73,11 +78,12 @@ AutoMaton *freeAutoMaton(AutoMaton *am) {
     freeVector(am->status);
     free(am);
 }
-
+/*这里也不对呀  应该是match才对*/
 void EdgeAssign(Edge *edge, Edge *fromEdge) {
     edge->nextEdge = fromEdge->nextEdge;
     edge->match.matchContent = copyVector(fromEdge->match.matchContent);
     edge->match.matchMode = fromEdge->match.matchMode;
+    edge->point = fromEdge->point;
 }
 
 void freeEdgeAssign(Edge *e) {
@@ -91,6 +97,7 @@ int setMatch(Match *match, enum MatchMode matchMode, LongChar *matchContent) {
     } else {
         ;
     }
+    /*这里也要改*/
     match->matchContent = INIT_VECTOR(DEFAULE_MATCH_CONTENT_SIZE, LongChar, EdgeAssign, freeEdgeAssign);
     return match->matchContent == NULL ? -1 : 0;
 }
@@ -119,10 +126,10 @@ int addStatus(AutoMaton *am) {
     Status s = {NULL};
     return appendVector(am->status, &s);
 }
-int addEdge(AutoMaton *am, int start, int end, enum MatchMode matchMode, LongChar MatchContent) {
+int addEdge(AutoMaton *am, int start, int end, enum MatchMode matchMode, LongChar MatchContent,int point) {
     Status *s = getReferenceVector(am->status, start);
     if (s->outEdge == NULL) {
-        s->outEdge = initEdge(matchMode, MatchContent);
+        s->outEdge = initEdge(matchMode, MatchContent,point);
         if (s->outEdge == NULL) {
             return -1;
         } else {
@@ -134,7 +141,7 @@ int addEdge(AutoMaton *am, int start, int end, enum MatchMode matchMode, LongCha
         while (e->nextEdge != NULL) {
             e = e->nextEdge;
         }
-        e->nextEdge = initEdge(matchMode, MatchContent);
+        e->nextEdge = initEdge(matchMode, MatchContent,point);
         if (e->nextEdge == NULL) {
             return -1;
         } else {
